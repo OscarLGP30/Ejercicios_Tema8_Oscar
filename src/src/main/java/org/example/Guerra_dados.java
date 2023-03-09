2768f83f1c99ee67a7ff9fce45c45ddd4ca0caee
OscarLGP30 <olegaper@myuax.com>
2023-03-09 19:12:57 +0100

    creo clase guerra de dados
    Took 55 seconds

---
+++ src/src/main/java/org/example/Guerra_dados.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Guerra_dados {
+
+    // Dos jugadores se enfrentan en una guerra de dados: cada jugador tiene un dado de seis caras y diez fichas.
+
+    //Cree una clase JugadorGuerra, que tenga como atributos de instancia el número de fichas, un dado (una instancia de la clase Dado) y el nombre del jugador.
+
+    // Agregue dentro de esta clase el resto de elementos necesarios.
+
+    // Cree un algoritmo principal usando esta clase.
+
+    
+}
